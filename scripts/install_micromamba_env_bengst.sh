eed7e5c402fcc7b05d9da6e53e9e8f60437ee817
B <ben.g.steinberg@gmail.com>
2023-10-31 18:27:58 +0200

    edit requirement files and env install scripts to work on zeus

--- scripts/install_micromamba_env_bengst.sh
+++ scripts/install_micromamba_env_bengst.sh
@@ -3,7 +3,7 @@ set -e  # Stop on error
 
 install_ucsc_tools_369() {
   # takes in conda env name and find conda bin
-  CONDA_BIN=$(conda run -n $1 bash -c "echo \$(dirname \$(which python))")
+  CONDA_BIN=$(micromamba run -n $1 bash -c "echo \$(dirname \$(which python))")
   curl -o "$CONDA_BIN/fetchChromSizes" "https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v369/fetchChromSizes"
   curl -o "$CONDA_BIN/wigToBigWig" "https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v369/wigToBigWig"
   curl -o "$CONDA_BIN/bedGraphToBigWig" "https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v369/bedGraphToBigWig"
@@ -42,33 +42,33 @@ micromamba create -f ${SH_SCRIPT_DIR}/requirements.spp_bengst.yml
 # conda install -n encd-chip-spp --no-deps --no-update-deps -y \
 #   readline==6.2 ncurses==5.9 -c conda-forge
 
-CONDA_BIN=$(conda run -n encd-chip-spp bash -c "echo \$(dirname \$(which python))")
+CONDA_BIN=$(micromamba run -n encd-chip-spp bash -c "echo \$(dirname \$(which python))")
 
 echo "$(date): Installing phantompeakqualtools in Conda environments..."
 RUN_SPP="https://raw.githubusercontent.com/kundajelab/phantompeakqualtools/1.2.2/run_spp.R"
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "curl -o $CONDA_BIN/run_spp.R $RUN_SPP && chmod +x $CONDA_BIN/run_spp.R"
 
 echo "$(date): Installing R packages in Conda environments..."
 CRAN="https://cran.r-project.org/"
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "Rscript -e \"install.packages('snow', repos='$CRAN')\""
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "Rscript -e \"install.packages('snowfall', repos='$CRAN')\""
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "Rscript -e \"install.packages('bitops', repos='$CRAN')\""
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "Rscript -e \"install.packages('caTools', repos='$CRAN')\""
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "Rscript -e \"install.packages('BiocManager', repos='$CRAN')\""
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "Rscript -e \"require('BiocManager'); BiocManager::install('Rsamtools'); BiocManager::install('Rcpp')\""
 
 echo "$(date): Installing R spp 1.15.5 in Conda environments..."
 SPP="https://cran.r-project.org/src/contrib/Archive/spp/spp_1.15.5.tar.gz"
 SPP_BASENAME=$(basename $SPP)
 curl -o "$CONDA_BIN/$SPP_BASENAME" "$SPP"
-conda run -n encd-chip-spp bash -c \
+micromamba run -n encd-chip-spp bash -c \
   "Rscript -e \"install.packages('$CONDA_BIN/$SPP_BASENAME')\""
 
 echo "$(date): Installing USCS tools (v369)..."
---
+++ scripts/update_micromamba_env_bengst.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e  # Stop on error
+
+SH_SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+SRC_DIR=${SH_SCRIPT_DIR}/../src
+
+PIPELINE_CONDA_ENVS=(
+  encd-chip
+  encd-chip-macs2
+  encd-chip-spp
+)
+chmod u+rx ${SRC_DIR}/*.py
+
+echo "$(date): Updating WDL task wrappers on each Conda environment..."
+for PIPELINE_CONDA_ENV in "${PIPELINE_CONDA_ENVS[@]}"
+do	
+  CONDA_BIN=$(dirname $(micromamba run -n ${PIPELINE_CONDA_ENV} which python))
+  echo -e "$(date): Transferring WDL task wrappers to ${CONDA_BIN}..."
+  cp -f ${SRC_DIR}/*.py ${CONDA_BIN}/
+done
