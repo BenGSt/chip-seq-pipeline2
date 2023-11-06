57051ac7c1b889fc5adb311aefa225fc2c249658
B <ben.g.steinberg@gmail.com>
2023-11-06 18:58:28 +0200

    add cromwell, woomtool, and default.config paths to not use ~/.caper/

--- run_on_zeus.sh
+++ run_on_zeus.sh
@@ -5,6 +5,7 @@ CROMWELL_DIR=/home/s.benjamin/bioinformatics_software/encode_pipelines/cromwell_
 
 main() {
   arg_parse "$@"
+  create_caper_home
   write_bakend_conf
   source /home/s.benjamin/other_software/conda/bin/activate caper
   caper hpc submit $ENCD_CHIP_DIR/chip.wdl \
@@ -14,11 +15,27 @@ main() {
     --local-loc-dir $PWD \
     --local-out-dir $PWD \
     --backend-file $(realpath ./zeus.backend.conf)
-    --cromwell $ENCD_CHIP_DIR/cromwell-*.jar
-    --woomtools $CROMWELL_DIR/woomtools-*.jar
-    --config $CROMWELL_DIR/zeus_default.conf
+}
+
+
+create_caper_home(){
+  #check if ~/.caper exists
+  if [ ! -d ~/.caper ]; then
+    echo "Creating ~/.caper and symlinking cromwell and woomtools"
+    mkdir ~/.caper
+    woomtools=$(ls $CROMWELL_DIR/woomtools-*.jar)
+    cromwell=$(ls $CROMWELL_DIR/cromwell-*.jar)
+    ln -s $ENCD_CHIP_DIR/zeus_default.conf default.conf
+    ln -s $CROMWELL_DIR/$woomtools $woomtools
+    ln -s $CROMWELL_DIR/$cromwell $cromwell
+  else
+    echo "~/.caper already exists for user $USER"
+    echo "If you are uable to run the pipeline, please delete ~/.caper and rerun this script"
+    echo
+  fi
 
 }
+
 
 write_bakend_conf(){
   echo "Writing backend.conf"
